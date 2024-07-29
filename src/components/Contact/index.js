71be17cd2019c22302a3e54b948ebb35f99da08e
Eksadwimada <exsaa.madha99@gmail.com>
2024-07-29 13:47:15 +0700

    first commit

---
+++ src/components/Contact/index.js
@@ -0,0 +1,211 @@
+import React, { useRef, useState } from 'react';
+import styled from 'styled-components';
+import { Snackbar } from '@mui/material';
+import Swal from 'sweetalert2';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: relative;
+    z-index: 1;
+    align-items: center;
+    @media (max-width: 960px) {
+        padding: 0px;
+    }
+        @media (max-width: 640px) {
+        padding: 0px 24px 0px 24px;
+    }
+`;
+
+const Wrapper = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    max-width: 1350px;
+    padding: 0px 0px 80px 0px;
+    gap: 12px;
+    @media (max-width: 960px) {
+        flex-direction: column;
+    }
+`;
+
+const Title = styled.div`
+font-size: 42px;
+text-align: center;
+font-weight: 600;
+margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+      margin-top: 12px;
+      font-size: 32px;
+  }
+      @media (max-width: 640px) {
+      margin-top: 20px;
+      font-size: 26px;
+    }
+`;
+
+const Desc = styled.div`
+    font-size: 18px;
+    text-align: center;
+    max-width: 600px;
+    color: ${({ theme }) => theme.text_secondary};
+    @media (max-width: 768px) {
+        margin-top: 12px;
+        font-size: 16px;
+    }
+    @media (max-width: 640px) {
+      font-size: 14px;
+    }
+`;
+
+const ContactForm = styled.form`
+  width: 95%;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.card};
+  padding: 32px;
+  border-radius: 16px;
+  box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
+  margin-top: 28px;
+  gap: 12px;
+`;
+
+const ContactTitle = styled.div`
+  font-size: 24px;
+  margin-bottom: 6px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 640px) {
+      font-size: 20px;
+    }
+`;
+
+const ContactInput = styled.input`
+  flex: 1;
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.text_secondary};
+  outline: none;
+  font-size: 18px;
+  color: ${({ theme }) => theme.text_primary};
+  border-radius: 12px;
+  padding: 12px 16px;
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.primary};
+  }
+    @media (max-width: 640px) {
+      font-size: 14px;
+    }
+`;
+
+const ContactInputMessage = styled.textarea`
+  flex: 1;
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.text_secondary};
+  outline: none;
+  font-size: 18px;
+  color: ${({ theme }) => theme.text_primary};
+  border-radius: 12px;
+  padding: 12px 16px;
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.primary};
+  }
+    @media (max-width: 640px) {
+      font-size: 14px;
+    }
+`;
+
+const ContactButton = styled.input`
+  width: 100%;
+  text-decoration: none;
+  text-align: center;
+  background: hsla(271, 100%, 50%, 1);
+  background: linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+  background: -moz-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+  background: -webkit-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+  padding: 13px 16px;
+  margin-top: 2px;
+  border-radius: 12px;
+  border: none;
+  color: ${({ theme }) => theme.text_primary};
+  font-size: 18px;
+  font-weight: 600;
+  @media (max-width: 640px) {
+      font-size: 14px;
+    }
+`;
+
+const Contact = () => {
+  const form = useRef();
+  const [result, setResult] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "980bada9-1954-4128-8e8d-d3b589255137");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      setOpen(true);
+      Swal.fire({
+        title: "Good job!",
+        text: "Your email has been sent successfully!",
+        icon: "success"
+      });
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+      setOpen(true);
+      Swal.fire({
+        title: "Error",
+        text: data.message,
+        icon: "error"
+      });
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Container>
+      <Wrapper>
+        <Title>Contact</Title>
+        <Desc>Feel free to reach out to me via email for any questions or opportunities!</Desc>
+        <ContactForm ref={form} onSubmit={onSubmit}>
+          <ContactTitle>Email Me 🚀</ContactTitle>
+          <ContactInput placeholder="Your Email" name="from_email" />
+          <ContactInput placeholder="Your Name" name="from_name" />
+          <ContactInput placeholder="Subject" name="subject" />
+          <ContactInputMessage placeholder="Message" rows="4" name="message" />
+          <ContactButton type="submit" value="Send" />
+        </ContactForm>
+        <Snackbar
+          open={open}
+          autoHideDuration={6000}
+          onClose={handleClose}
+          message={result}
+        />
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Contact;
---
+++ src/data/constants.js
@@ -0,0 +1,272 @@
+export const Bio = {
+    name: "Eksa Dwi Mada",
+    roles: [
+      "Android Developer",
+      "UI/UX Designer",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/Eksadwimada",
+    resume:
+    "https://drive.google.com/file/d/1myoqYb8Ai19Xo5oES0yisEvnhh2svv1E/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/eksa-dwi-mada/",
+    insta: "https://www.instagram.com/eksadmd/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+          name: "Flutter",
+          image:
+            "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "Firebase",
+          image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+        },
+      ],
+    },
+    {
+      title: "Android",
+      skills: [
+        {
+          name: "Kotlin",
+          image:
+            "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+        },
+        {
+          name: "Android Studio",
+          image:
+            "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Figma",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/data-f7b0e.appspot.com/o/CrowdeMock.png?alt=media&token=ef8f9c82-9ea4-4e28-b021-f97a9752f6cb",
+      role: "UI/UX Designer - Redesigner Website Crowde",
+      company: "PT Crowde",
+      date: "Nov 2023 - Dec 2023",
+      desc: "This project is training from the Merdeka Campus in the Independent Study Program and Skillvul UI/UX Design. CROWDE as Challenge partner. We do not work for or are professionally contracted by CROWDE.",
+      skills: [
+        "Brainstorming",
+        "Design Thinking",
+        "UI Design",
+        "UX Design",
+        "Prototyping",
+        "Figma",
+      ],
+      doc: "https://firebasestorage.googleapis.com/v0/b/data-f7b0e.appspot.com/o/CROWDE.png?alt=media&token=5ac242f8-6145-45bb-b229-bf661aad42ed",
+    },
+    {
+      id: 1,
+      img: "https://firebasestorage.googleapis.com/v0/b/data-f7b0e.appspot.com/o/AndroMock.png?alt=media&token=3268e19a-c3c6-4cac-9be8-05b13528db1d",
+      role: "Android Developer",
+      company: "Serayu Larangan Village",
+      date: "June 2023 - Mei 2024",
+      desc: "This project is an Android-based application for tourism in the village of Serayu Ban to regulate the level of crowds of visitors at each tourist attraction by scanning barcodes.",
+      skills: [
+        "Android Studio",
+        "Kotlin",
+        "Firebase",
+        "XML",
+        "Figma",
+      ],
+      doc: "https://firebasestorage.googleapis.com/v0/b/data-f7b0e.appspot.com/o/Serayu.png?alt=media&token=0ca5acae-ca42-45f7-916f-8e38ed99925a",
+    },
+    {
+      id: 2,
+      img: "https://firebasestorage.googleapis.com/v0/b/data-f7b0e.appspot.com/o/SkilvulMock.png?alt=media&token=4a650810-71a2-44c9-bed9-7a4923291518",
+      role: "Study Independent - UI/UX Design Mastery",
+      company: "Skilvul - PT. Impactbyte Education Technology",
+      date: "Aug 2022 - Dec 2022",
+      desc: "Has successfully completed his duties at PT. Impactbyte Education Technology in Skilvul #Tech4Impact program with UI/UX Design Mastery learning track",
+      skills: [
+        "Brainstorming",
+        "Design Thinking",
+        "UI Design",
+        "UX Design",
+        "Prototyping",
+        "Testing",
+        "Figma",
+        "Team Work",
+      ],
+      doc: "https://firebasestorage.googleapis.com/v0/b/data-f7b0e.appspot.com/o/Sertifikat%20MSIB.jpg?alt=media&token=40f8edfd-209f-4176-9b18-b81bd5e85de8",
+    },
+    {
+      id: 3,
+      img: "https://firebasestorage.googleapis.com/v0/b/data-f7b0e.appspot.com/o/LspMock.png?alt=media&token=1c108bf2-12ea-4c74-9019-5c1d58aeba99",
+      role: "Junior Mobile Programmer",
+      company: "Lembaga Sertifikasi Profesi Informastika - BNSP",
+      date: "Apr 2024 - Apr 2027",
+      desc: "This certificate is proof that the assessment has been carried out with a competent recommendation that I as a participant have passed",
+      skills: [
+        "Android Studio",
+        "Kotlin",
+        "Firebase",
+        "XML",
+        "Mobile Security",
+        "Mobile Computing",
+      ],
+      doc: "https://firebasestorage.googleapis.com/v0/b/data-f7b0e.appspot.com/o/Sertifikat%20Serkom.jpg?alt=media&token=3b5f3267-92c8-40dc-b267-11cb0fcfdc1a",
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/data-f7b0e.appspot.com/o/TelU.png?alt=media&token=fdaf2abc-3dfe-4f14-bf5a-e3513fd6d0e5",
+      school: "Telkom University Purwokerto",
+      date: "Sep 2020 - Jun 2024",
+      grade: "A",
+      desc: "Currently I have completed a Bachelor's degree in Informatics Engineering at Telkom University. I have completed 4 semesters and have a final GPA of 3.68. I have taken courses in Algorithm Structure, Object Oriented Programming, Database Management Systems, Operating Systems, Computer Networks, etc.",
+      degree: "S1 Informatics Engineering",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Redesign Website Crowde",
+      date: "Oct 2022 - Nov 2022",
+      description:
+        "Crowde is a fintech startup focused on agriculture that provides a platform to support farmers through agricultural capital and technological innovation. Crowde provides special capital services for farmers in the form of production facilities and labor financing. Providing guidance when cultivating and returning capital in the form of harvest results.",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/data-f7b0e.appspot.com/o/ProjekCrowde.png?alt=media&token=122dba49-9dc4-4498-9b93-6df9c4090f64",
+      tags: ["Design Thinking", "UI/UX Design", "Brainstorming", "Prototyping", "Figma"],
+      category: "uiux design",
+      github: "https://www.figma.com/design/KkVnzVq72VyqqzK66MNyoH/Crowde?node-id=1-313&t=3Yh5Oqh7djvzWisI-1",
+      webapp: "https://www.figma.com/proto/KkVnzVq72VyqqzK66MNyoH/Crowde?node-id=3-704&t=zoZm0xsgjhDYDUvO-1&scaling=min-zoom&content-scaling=fixed&page-id=1%3A313&starting-point-node-id=3%3A704",
+      member: [
+        {
+          name: "Eksa Dwi Mada",
+          img: "https://avatars.githubusercontent.com/u/92199701?v=4",
+          linkedin: "https://www.linkedin.com/in/eksa-dwi-mada-59b492241/",
+          github: "https://github.com/Eksadwimada",
+        },
+        {
+          name: "Fajar Arifandi",
+          img: "https://avatars.githubusercontent.com/u/101867497?v=4",
+          github: "https://github.com/FajarArifandi",
+        },
+        {
+          name: "Ela Risti Khasanah",
+          img: "https://avatars.githubusercontent.com/u/80968029?v=4",
+          github: "https://github.com/Ela1811",
+        },
+      ],
+    },
+    {
+      id: 1,
+      title: "UI Design Login Register",
+      date: "Jan 2023",
+      description:
+        "The page design for logging in and registering is made with a dominant green color palette.",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/data-f7b0e.appspot.com/o/UIMock.png?alt=media&token=b1a6d0f5-c06c-4615-973e-7fcdd62092f1",
+      tags: ["Figma"],
+      category: "uiux design",
+      github: "https://www.figma.com/design/IZurUq1W4F3BNQqheKtt6p/UI-Login-Page?node-id=0-1&t=pcR1HIcS7vn1noPv-1",
+      webapp: "https://www.figma.com/design/IZurUq1W4F3BNQqheKtt6p/UI-Login-Page?node-id=0-1&t=pcR1HIcS7vn1noPv-1",
+    },
+    {
+      id: 2,
+      title: "UI Design Sinau App",
+      date: "Apr 2023",
+      description:
+        "Sinau application design Digital learning as an alternative in the world of education that can provide learning services and resources that are easy and quickly accessible.",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/data-f7b0e.appspot.com/o/SinauMock.png?alt=media&token=7951e630-1c72-4ac5-83e5-f7890b292e53",
+      tags: ["Figma", "Design System", "Prototyping"],
+      category: "uiux design",
+      github: "https://www.figma.com/design/TE9txaMAIyI5xWwLK5rMqD/Design-System-%26-UI-Design?node-id=3-4&t=1uKcEwRWoVl228mz-0",
+      webapp: "https://www.figma.com/proto/TE9txaMAIyI5xWwLK5rMqD/Design-System-%26-UI-Design?node-id=74-1997&t=1uKcEwRWoVl228mz-0&scaling=scale-down&content-scaling=fixed&page-id=3%3A4&starting-point-node-id=68%3A2032",
+    },
+    {
+      id: 4,
+      title: "Serayu Larangan App",
+      date: "Apr 2023 - May 2024",
+      description:
+        "This Android-based tourism application is used to detect the crowd level of a tourist attraction in Serayu Larangan village.",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/data-f7b0e.appspot.com/o/Serayu.png?alt=media&token=0ca5acae-ca42-45f7-916f-8e38ed99925a",
+      tags: ["Android Studio", "Kotlin", "XML", "Firebase", "Figma"],
+      category: "android app",
+      github: "https://github.com/Eksadwimada/SerayuLaranganApp",
+      webapp: "https://github.com/Eksadwimada/SerayuLaranganApp",
+    },
+    {
+      id: 5,
+      title: "Web Portofolio",
+      date: "jun 2024",
+      description:
+        "A personal portfolio website is an online platform used to showcase an individual's work, skills, experience and achievements in various fields, especially in a professional and creative context.",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/data-f7b0e.appspot.com/o/webme.png?alt=media&token=e2343529-38c8-45b9-9298-ef873f6fd15b",
+      tags: ["Visual Studio Code", "React Js", "Firebase", "Figma"],
+      category: "web app",
+      github: "https://github.com/Eksadwimada/",
+      webapp: "https://github.com/Eksadwimada/",
+    },
+  ];
+  
